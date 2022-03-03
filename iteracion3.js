a453a13cbed720dfec904ef522b76a01de87b496
Ivan <ivanferrera67@gmail.com>
2022-03-03 22:15:18 +0100

    He hecho las iteraciones

---
+++ iteracion3.js
@@ -0,0 +1,9 @@
+const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar'];
+
+const forOf = (list) => {
+    for (const value of list) {
+        console.log(value)
+    }
+}
+
+forOf(placesToTravel);
